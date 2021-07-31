e30eff231ce398ac8079e24a16a309b4bf003d93
bawowtsn <mactsn@yahoo.com>
2021-07-31 08:37:22 -0500

    completed reactive nav bar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,11 +1,72 @@
-import React from 'react'
+import React,{ useState } from 'react'
+import Link from 'next/link'
+import { a, NavButton, NavLinks, ReactiveNavLinks, Logo, NavBarContainer, ReactiveNavContainer } from '../styles/Navbar.module.css'
 
 const Navbar = () => {
+    const [nav, setNav] = useState ("false");
+    
+    
+    const ReactiveNav = () => {
+        setNav(!nav);
+    };
+    
     return (
-        <div>
-            <h1>Navbar</h1>
+
+        <div className={nav ? NavBarContainer : ReactiveNavContainer}>
+            <a className={Logo} href='' > &#8492;&#10070;Code&#9854; </a>
+            <nav className={nav ? NavLinks : ReactiveNavLinks}>
+                <Link href="/" className={Logo}> Tester</Link>
+                 <a href='' className={a} >Technologies</a> 
+                 <a href='' className={a} >Projects</a> 
+                 <a className={a} >Timeline</a> 
+            </nav>
+            <button className={NavButton} onClick={ReactiveNav}>&#9776;</button>
+
         </div>
+
     )
 }
+
+// style={{backgroundColor:"green"}} 
+
+ 
+
+// const navStyles = {
+//     navbar: {
+//         backgroundColor: '#f1f1f1',
+//         padding: '50px 10px',
+//         display:'flex',
+//         alignItems:'center',
+//         // justifyContent:'center',
+//         width:'100%',
+//         zIndex:'99',
+//         '& span': {
+//             color: 'blue',
+//             textDecoration: 'none',
+//             padding: '12px 16px',
+//         },
+//         '& nav': {
+//             color: 'black',
+//             textDecoration: 'none',
+//             padding: '12px 16px',
+//             justifyContent:'center',
+//         },
+//     },
+
+//     a: {
+//         color: 'blue',
+//         textDecoration: 'none',
+//         padding: '12px 16px',
+//     },
+    
+//     logo: {
+//         justifyContent:'flex-start',
+//         fontSize:'35px',
+//         fontWeight: 'bold',
+//         padding: '12px 16px',
+//         transition: '4s',
+
+//     },
+// }
 
 export default Navbar
--- components/StyleComponents.js
+++ components/StyleComponents.js
@@ -7,11 +7,66 @@ export const Container = styled.div`
     margin: auto;
 `;
 
+export const Row = styled.div`
+    max-width: 1280px;
+    width: 100%;
+    margin: auto;
+    display:flex;
+    align-items:center;
+    flex-wrap:wrap;
+    justify-content:center;
+`;
+
 export const FooterContainer = styled.div`
     width: 100%;
+    background-color:none;
     height: 100px;
     border-top: 1px solid #eaeaea;
     display: flex;
     justify-content: center;
     align-items: center;
 `;
+
+export const NavBarContainer = styled.div `
+        background-color: #f1f1f1;
+        padding: 50px 10px;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content:space-evenly;
+        align-items:center;
+        zIndex:99;
+`;
+
+export const Logo = styled.a `
+        font-size:35px;
+        font-weight: bold;
+        padding: 0px 0px;
+        transition: 4s;
+        color: blue;
+`;
+
+export const NavLinks = styled.nav `
+        align-items:center;
+        
+        @media screen and (max-width: 768px) {
+            display: none;
+        }
+`;
+
+
+
+// export const NavRef = styled.a `
+//         color: black;
+//         textDecoration: none;
+//         padding: 12px 16px;
+// `
+
+
+export const NavButton = styled.button `
+        background-color:none;
+        border: 2px solid black;
+        border-radius: 4px;
+        padding: 12px 16px;
+        cursor: pointer;
+`;
+
